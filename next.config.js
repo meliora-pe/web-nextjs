415a30585c7605eec081ac4684a8ff3c5ca17b4b
marcoariasm <marcoarias@outlook.com>
2025-08-16 17:12:35 -0500

    [S1-MSD1] fix next export packag.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
+    output: 'export',
     trailingSlash: true,
     reactStrictMode: true,
     swcMinify: true,
