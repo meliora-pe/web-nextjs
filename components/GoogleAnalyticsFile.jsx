9fbfc3c4891962fd0b32ffcf260d5e300ef2664a
marcoariasm <marcoarias@outlook.com>
2025-08-22 10:25:07 -0500

    [S1-MSD1] install GTM on body tag

--- components/GoogleAnalyticsFile.jsx
+++ components/GoogleAnalyticsFile.jsx
@@ -1,14 +1,14 @@
-"use client";
+'use client';
 
-import { useEffect } from "react";
-import { initializeGA } from "../lib/google-analytics";
+import { useEffect } from 'react';
+import { initializeGA } from '../lib/google-analytics';
 
 export default function GoogleAnalytics() {
-  useEffect(() => {
-    if (!window.GA_INITIALIZED) {
-      initializeGA();
-      window.GA_INITIALIZED = true;
-    }
-  }, []);
-  return null
+    useEffect(() => {
+        if (!window.GA_INITIALIZED) {
+            initializeGA(process.env.NEXT_PUBLIC_GA_ID);
+            window.GA_INITIALIZED = true;
+        }
+    }, []);
+    return null;
 }
