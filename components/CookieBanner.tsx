4cfb6782c10d792c455ce4d30320e93607f02046
marcoariasm <marcoarias@outlook.com>
2025-08-21 21:52:58 -0500

    [S1-MSD1] update consent GA4

--- components/CookieBanner.tsx
+++ components/CookieBanner.tsx
@@ -2,57 +2,59 @@
 
 'use client';
 
-import { getLocalStorage, setLocalStorage } from '../lib/storageHelper';
-import { useState, useEffect } from 'react';
-import Link from 'next/link'
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
-export default function CookieBanner(){
+import { getLocalStorage, setLocalStorage } from '../lib/storageHelper';
+
+export default function CookieBanner() {
     const [cookieConsent, setCookieConsent] = useState(false);
 
-    useEffect (() => {
-        const storedCookieConsent = getLocalStorage("cookie_consent", null)
+    useEffect(() => {
+        const storedCookieConsent = getLocalStorage('cookie_consent', null);
 
-        setCookieConsent(storedCookieConsent)
+        setCookieConsent(storedCookieConsent);
     }, [setCookieConsent]);
 
     useEffect(() => {
-        const newValue = cookieConsent ? 'granted' : 'denied'
+        const newValue = cookieConsent ? 'granted' : 'denied';
 
-        window.gtag("consent", 'update', {
-            'analytics_storage': newValue
+        window.gtag('consent', 'update', {
+            analytics_storage: newValue,
+            ad_storage: newValue,
+            ad_personalization: newValue,
+            ad_user_data: newValue,
         });
 
-        setLocalStorage("cookie_consent", cookieConsent)
+        setLocalStorage('cookie_consent', cookieConsent);
 
         document.body.style.overflow = cookieConsent === null ? 'hidden' : 'auto';
-
     }, [cookieConsent]);
 
     return (
-        <div className={`bg-gray my-10 mx-auto max-w-max md:max-w-screen-sm
-                        fixed bottom-0 left-0 right-0
-                        z-50 flex px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row
-                        rounded-lg shadow-md
-                        ${cookieConsent != null ? "hidden" : "flex"}` }
+        <div
+            className={`fixed bottom-0 left-0 right-0 z-50
+                        mx-auto my-10 flex max-w-max
+                        flex-col items-center justify-between rounded-lg bg-gray px-3 py-3 shadow-md sm:flex-row
+                        md:max-w-screen-sm md:px-4
+                        ${cookieConsent != null ? 'hidden' : 'flex'}`}
         >
-            <div className='text-center text-white'>
-                <Link href="/cookies"><p>Usamos <span className='font-bold text-black'>cookies</span> en este sitio web.</p></Link>
+            <div className="text-center text-white">
+                <Link href="/cookies">
+                    <p>
+                        Usamos <span className="font-bold text-black">cookies</span> en este sitio web.
+                    </p>
+                </Link>
             </div>
 
-            
-            <div className='flex gap-2'>
-                <button 
-                    className='px-5 py-2 text-black rounded-lg border-gray-900 hover:bg-white hover:font-bold'
-                    onClick={() => setCookieConsent(false)}
-                    >
-                        Rechazar
+            <div className="flex gap-2">
+                <button className="border-gray-900 rounded-lg px-5 py-2 text-black hover:bg-white hover:font-bold" onClick={() => setCookieConsent(false)}>
+                    Rechazar
                 </button>
-                <button 
-                    className='hover:bg-white hover:font-bold text-black px-5 py-2 rounded-lg'
-                    onClick={() => setCookieConsent(true)}
-                    >
-                        Permitir Cookies
+                <button className="rounded-lg px-5 py-2 text-black hover:bg-white hover:font-bold" onClick={() => setCookieConsent(true)}>
+                    Permitir Cookies
                 </button>
-            </div>   
+            </div>
         </div>
-    )}
+    );
+}
