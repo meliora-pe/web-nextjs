92d79a936518a3a55b517997922131d5c4141f6f
marcoariasm <marcoarias@outlook.com>
2026-01-20 10:57:17 -0500

    fix data aos dolor_y_solucion

--- pages/landings/ad.tsx
+++ pages/landings/ad.tsx
@@ -79,7 +79,12 @@ const Ad = () => {
                             <div className="relative mx-auto h-[320px] w-full max-w-[520px] sm:h-[420px]">
                                 {/* card 1 */}
                                 <div className="absolute top-9 w-[56%] rotate-[-3deg]">
-                                    <div className="relative overflow-hidden rounded-2xl shadow-lg ring-1 ring-gray">
+                                    <div
+                                        className="relative overflow-hidden rounded-2xl shadow-lg ring-1 ring-gray"
+                                        data-aos="fade-right"
+                                        data-aos-duration="600"
+                                        data-aos-delay="100"
+                                    >
                                         <Image
                                             src="/assets/images/landings/ad/blanqueamiento-0.png"
                                             alt="Inseguridad al sonreír"
@@ -92,7 +97,12 @@ const Ad = () => {
                                 </div>
                                 {/* card 2 */}
                                 <div className="absolute right-0 top-16 w-[62%] rotate-[2deg]">
-                                    <div className="relative overflow-hidden rounded-2xl shadow-lg ring-1 ring-gray">
+                                    <div
+                                        className="relative overflow-hidden rounded-2xl shadow-lg ring-1 ring-gray"
+                                        data-aos="fade-down"
+                                        data-aos-duration="600"
+                                        data-aos-delay="400"
+                                    >
                                         <Image
                                             src="/assets/images/landings/ad/blanqueamiento-4.jpg"
                                             alt="Taparse la sonrisa por manchas"
@@ -104,7 +114,12 @@ const Ad = () => {
                                 </div>
                                 {/* card 3 */}
                                 <div className="absolute bottom-4 left-9 w-[42%] rotate-[-2deg]">
-                                    <div className="relative overflow-hidden rounded-2xl shadow-xl ring-1 ring-gray">
+                                    <div
+                                        className="relative overflow-hidden rounded-2xl shadow-xl ring-1 ring-gray"
+                                        data-aos="fade-up"
+                                        data-aos-duration="600"
+                                        data-aos-delay="700"
+                                    >
                                         <Image
                                             src="/assets/images/landings/ad/blanqueamiento-5.png"
                                             alt="Sonrisa más blanca y natural"
@@ -116,7 +131,12 @@ const Ad = () => {
                                 </div>
                                 {/* card 4 */}
                                 <div className="absolute bottom-5 right-10 w-[44%] rotate-[4deg]">
-                                    <div className="relative overflow-hidden rounded-2xl shadow-md ring-1 ring-gray">
+                                    <div
+                                        className="relative overflow-hidden rounded-2xl shadow-md ring-1 ring-gray"
+                                        data-aos="fade-left"
+                                        data-aos-duration="600"
+                                        data-aos-delay="1000"
+                                    >
                                         <Image
                                             src="/assets/images/landings/ad/blanqueamiento-3.png"
                                             alt="Manchas por café o té"
@@ -170,8 +190,8 @@ const Ad = () => {
                                 <p className="pl-2 font-raleway font-semibold text-black">{AboveTheFold.cta.text}</p>
                             </Link>
                         </div>
-                        <div className="mt-3 flex-1 overflow-hidden rounded-[25px] sm:mt-0" data-aos="fade" data-aos-duration="500" data-aos-delay="100">
-                            <img src="/assets/images/landings/ad/blanqueamiento-2.jpg" alt="" />
+                        <div className="mt-3 flex-1 overflow-hidden rounded-[25px] sm:mt-0" data-aos="fade" data-aos-duration="500">
+                            <img src="/assets/images/landings/ad/blanqueamiento-2.jpg" alt="blanqueamiento dental" />
                         </div>
                     </div>
                 </div>
