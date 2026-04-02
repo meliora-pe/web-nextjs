c9ef581985cbd85f4989f8d878bae32eaa11868f
marcoariasm <marcoarias@outlook.com>
2026-04-02 12:39:37 -0500

    fix consent analytics set to granted

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -30,7 +30,7 @@ export default function GoogleAnalytics({ googleAnalyticsId }: { googleAnalytics
                 gtag('js', new Date());
 
                 gtag('consent', 'default', {
-                    'analytics_storage': 'denied'
+                    'analytics_storage': 'granted'
                 });
                 
                 gtag('config', '${googleAnalyticsId}', {
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -72,14 +72,14 @@ export default function App({ Component, pageProps, ...appProps }: AppProps) {
                             <link href="/assets/images/favicon-dark.png" rel="icon" media="(prefers-color-scheme: dark)" />
                             <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
                             <FacebookPixelEvents />
-                            <Script id="google-ads-cta-appointment-click" strategy="afterInteractive">
-                                gtag('event', 'cta_appointment_click',{' '}
-                                {
-                                    // <event_parameters>
-                                }
-                                );
-                            </Script>
                         </Head>
+                        {/* <Script id="google-ads-cta-appointment-click" strategy="afterInteractive">
+                            gtag('event', 'cta_appointment_click',{' '}
+                            {
+                                // <event_parameters>
+                            }
+                            );
+                        </Script> */}
                         <GoogleAnalytics googleAnalyticsId={process.env.NEXT_PUBLIC_GA_ID || ''} />
                         <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID || ''} />
                         <Component {...pageProps} />
